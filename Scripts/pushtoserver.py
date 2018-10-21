9ff31c5a675f5be10dac941289495a6d928aeccb
Arne Goerlitz <a-goerlitz@hotmail.de>
2018-10-21 14:50:46 +0200

    [add] Kommentare hinzugefügt

--- Scripts/pushtoserver.py
+++ Scripts/pushtoserver.py
@@ -3,19 +3,20 @@ import subprocess
 import os
 
 i=inotify.adapters.Inotify()
-filepath="/home/pi/work/data"
-serverpath="generalmine@95.208.136.144:work/data"
-i.add_watch(filepath)
+filepath="/home/pi/work/data"													#config file Path
+serverpath="generalmine@95.208.136.144:work/data"								#config Habitat Server
+i.add_watch(filepath)															#subscribe to server event in folder
+
 
 try:
     for event in i.event_gen():
-        if event is not None:
+        if event is not None:													#if a new event exists
             (header, type_names, watch_path, filename) = event
-            if "IN_CLOSE_WRITE" in type_names:
+            if "IN_CLOSE_WRITE" in type_names:									#if new closed file in subscribed folder
                 print("File created...")
-                subprocess.call(["scp", filepath + "/" + filename, serverpath])
+                subprocess.call(["scp", filepath + "/" + filename, serverpath])	#push file to Habitat Server
                 print("File uploaded")
-                os.remove(filepath+"/"+filename)
+                os.remove(filepath+"/"+filename)								#remove file from pseudo-satellites
                 print("File deleted")
 finally:
-    i.remove_watch(filepath)
+    i.remove_watch(filepath)													#reset watch
--- Scripts/record.sh
+++ Scripts/record.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-lsusb -d 0bda:2838 > /dev/null
-directory="data/"
+lsusb -d 0bda:2838 > /dev/null																#DVBT-device connected
+directory="data/"																			
 separator="_"
 fileending=".dat"
-while [ $? == 0 ]
+while [ $? == 0 ]																			#while device is connected
 do
-    timed=$(date +%s)
+    timed=$(date +%s)																		#set time stemp
     hostn=$(hostname)
-    rtl_sdr -f 446000000 -g 30 -n 122880000 "$directory$hostn$separator$timed$fileending"
+    rtl_sdr -f 446000000 -g 30 -n 122880000 "$directory$hostn$separator$timed$fileending"	#record
     echo "Recorded" 
     lsusb -d 0bda:2838 > /dev/null
 done
